e855d8b273318043161dd66d4c8688e659fb5083
Andrija <andreccccc12@gmail.com>
2020-09-18 17:51:46 +0200

    Upadted cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -4,7 +4,7 @@ import { addItemToCart } from './cart.utils';
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
-}
+};
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
@@ -12,15 +12,15 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
-            }
+            };
         default:
             return state;
     }
-}
+};
 
 export default cartReducer;
